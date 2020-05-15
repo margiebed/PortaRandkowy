0a4c35f365957ec4d926e49cdd47cae3b648c501
margiebed <margiebed@gmail.com>
2020-05-15 14:46:39 +0200

    UtworzenieProjektu Portal Randkowy WEB API

---
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PortalRandkowy.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAction()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
